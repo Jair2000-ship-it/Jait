bb1ea51ade3e2f1ac1c089dd2c1af11e4d2bbd38
jair <luisjairtorresfalcones@gmailcom>
2021-06-03 19:42:27 -0500

    mi primer

---
+++ Python/ejercicio002.py
@@ -0,0 +1,7 @@
+
+
+
+lado_uno = float (input("Ingrese el primer lado del cuadrado"))
+lado_dos = float (input("Ingrese el segundo lado del cuadrado"))
+lado_tres = float (input("Ingrese el tercer lado del cuadrado"))
+lado_cuatro = float (input("Ingrese el cuarto lado del cuadrado"))

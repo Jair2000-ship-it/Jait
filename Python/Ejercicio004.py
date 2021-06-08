c9cc7d5b4413b4a09c9ddd25273f755ec0bd8b50
jair <luisjairtorresfalcones@gmailcom>
2021-06-07 20:22:02 -0500

    ejercicios deber

--- Python/Ejercicio004.py
+++ Python/Ejercicio004.py
@@ -1,1 +1,9 @@
-#debocalcular el perimetrogit 
+pi = 3.1416
+radio = float(input ("Introduce el radio del circulo: "))
+
+area =pi * (radio**2)
+perimetro = 2 * radio * pi
+
+
+print(f"El area del ciculo es  {area}") 
+print (f"El perimetro del circulo es {perimetro}")
--- Python/ejercicio003.py
+++ Python/ejercicio003.py
@@ -1,1 +1,5 @@
-#este es mi primer comit
+base = float(input ("ingrese la base del triangulo: "))
+altura = float (input ("Ingrese la altura del triangulo: "))
+
+print(f"El resultado del area del triangulo es: {((base * altura)/2)}")
+print(f"El resultado del perimetro del triangulo es: {(base * 3)} ")
